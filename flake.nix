13fe023f6b6bf42f8f096dc2b5a1e650bfc71943
David Asabina <vid@bina.me>
2023-09-23 16:59:54 +0900

    refactor: Use flake-util to make platform-agnostic

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
   description = "Utility to access Linux-native filesystems on Windows and macOS";
 
-  # TODO: Rewrite to use flake-utils
-  # Currently this was a quick-and-dirty writeup for x86_64-darwin but
-  # aarch64-darwin will be needed for the M2 MBA that I bought a few days back.
-  outputs = { self, nixpkgs }:
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       # Build through: nix build .
-      packages.x86_64-darwin.default = pkgs.buildGo121Module rec {
+      packages.default = pkgs.buildGo121Module rec {
         name = "linsk";
         src = builtins.path {
           path = ./.;
@@ -22,7 +21,7 @@
       };
 
       # Enter through: nix develop
-      devShells.x86_64-darwin.default = pkgs.mkShell {
+      devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           nixpkgs-fmt
         ];
@@ -30,5 +29,6 @@
           nixpkgs-fmt *.nix;
         '';
       };
-    };
+    }
+  );
 }
